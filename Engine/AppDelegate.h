f0b1d9d798585155c5555764f93582e9c531792b
Aurora <aurora@aventine.se>
2018-07-22 11:00:10 +0200

    Initial Commit

---
+++ Engine/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Engine
+//
+//  Created by Aurora on 22/07/2018.
+//  Copyright © 2018 Aventine. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Engine/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Engine
+//
+//  Created by Aurora on 22/07/2018.
+//  Copyright © 2018 Aventine. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
